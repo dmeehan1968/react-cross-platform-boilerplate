de94cd0bf90f406fe3a32b2869e9323de0535ec6
Dave Meehan <dave_meehan@replicated.co.uk>
2018-01-15 18:25:04 +0000

    Extract Document into own module

---
+++ src/sketchapp/components/Document.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Text, View, Artboard } from 'react-sketchapp'
+import App from './App'
+
+const devices = [
+  { name: 'iPhone SE', width: 320, height: 568 },
+  { name: 'iPhone 7', width: 375, height: 667 },
+  { name: 'iPhone 7+', width: 414, height: 736 },
+]
+
+const vars = {
+  documentPadding: 50,
+  margin: 10
+}
+
+const documentWidth = devices.reduce((sum, device) => sum + (device.width + vars.margin), 0) + (vars.documentPadding * 2)
+
+export default Document = () => (
+
+  <Artboard style={
+    {
+      width: documentWidth,
+      padding: vars.documentPadding,
+      flexDirection: 'row',
+      justifyContent: 'space-between'
+    }
+  }>
+    {devices.map(device => (
+      <View key={device.name} >
+        <Text style={
+          {
+            marginTop: vars.margin,
+            marginBottom: vars.margin*2,
+          }
+        }>{device.name} ({device.width}x{device.height})</Text>
+        <View style={
+          {
+            width: device.width,
+            height: device.height,
+            borderWidth: 1,
+            borderColor: 'red',
+          }
+        }>
+          <App />
+        </View>
+      </View>
+    ))}
+  </Artboard>
+)
--- src/sketchapp/index.js
+++ src/sketchapp/index.js
@@ -1,52 +1,6 @@
 import React from 'react'
-import { render, Text, View, Artboard } from 'react-sketchapp'
-import App from './components/App'
-
-const devices = [
-  { name: 'iPhone SE', width: 320, height: 568 },
-  { name: 'iPhone 7', width: 375, height: 667 },
-  { name: 'iPhone 7+', width: 414, height: 736 },
-]
-
-const vars = {
-  documentPadding: 50,
-  margin: 10
-}
-
-const documentWidth = devices.reduce((sum, device) => sum + (device.width + vars.margin), 0) + (vars.documentPadding * 2)
-
-const Document = () => (
-
-  <Artboard style={
-    {
-      width: documentWidth,
-      padding: vars.documentPadding,
-      flexDirection: 'row',
-      justifyContent: 'space-between'
-    }
-  }>
-    {devices.map(device => (
-      <View key={device.name} >
-        <Text style={
-          {
-            marginTop: vars.margin,
-            marginBottom: vars.margin*2,
-          }
-        }>{device.name} ({device.width}x{device.height})</Text>
-        <View style={
-          {
-            width: device.width,
-            height: device.height,
-            borderWidth: 1,
-            borderColor: 'red',
-          }
-        }>
-          <App {...device} />
-        </View>
-      </View>
-    ))}
-  </Artboard>
-)
+import { render } from 'react-sketchapp'
+import Document from './components/Document'
 
 export default () => {
 
