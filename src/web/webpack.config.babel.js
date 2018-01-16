fdcd3b1ad85f10967cd2f45bf0a6804a2331bfda
Dave Meehan <dave_meehan@replicated.co.uk>
2018-01-16 19:53:40 +0000

    Create webpack config

---
+++ src/web/webpack.config.babel.js
@@ -0,0 +1,46 @@
+import path from 'path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+
+export default {
+
+  entry: path.resolve(__dirname, 'index.js'),
+
+  output: {
+    filename: 'build.js',
+    path: path.resolve(__dirname, '../../dist')
+  },
+
+  devServer: {
+    contentBase: path.resolve(__dirname, '../../dist')
+  },
+
+  plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'index.html'),
+        filename: 'index.html'
+      })
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            babelrc: false,
+            presets: [ 'env', 'react' ],
+            sourceMaps: true
+          }
+        }
+      }
+    ]
+  },
+
+  resolve: {
+       alias: {
+           "react-native": "react-native-web"
+       }
+   }
+}
